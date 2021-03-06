3f2fa8d0455fcc065121169eba705398c16b5653
Kasthurid25 <kasthurid25@gmail.com>
2021-03-06 14:11:40 +0530

    level 3

--- Mytrip/src/main/java/org/Make/Mytrip/LoginAc.java
+++ Mytrip/src/main/java/org/Make/Mytrip/LoginAc.java
@@ -1,19 +1,26 @@
 package org.Make.Mytrip;
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class LoginAc {
-	WebDriver driver =new FirefoxDriver();
+	WebDriver driver = new FirefoxDriver();
 	public void log()
 	{
-	driver.get("https://www.makemytrip.com/");
+		driver.get("https://www.makemytrip.com/");
+		driver.manage().window().maximize(); 
+		driver.quit();
+
 	}
-public static void main(String[] args)	
-{
-	 System.setProperty("webdriver.gecko.driver","C:\\Users\\Multimedia\\Downloads\\geckodriver-v0.29.0-win32\\geckodriver.exe"); 
-	LoginAc	l1= new LoginAc();
-	l1.log();
+	
+	
+
+	public static void main(String[] args) {
+		
+		 System.setProperty("webdriver.gecko.driver","C:\\Users\\Multimedia\\Downloads\\geckodriver-v0.29.0-win32\\geckodriver.exe"); 
+			LoginAc m=new LoginAc();
+			m.log();
 	
 }
 
